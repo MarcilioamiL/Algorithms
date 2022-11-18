b250ea5ee10c41f4367fcc7ea3e6cf389f43b550
MarcilioamiL <marcilioaugusto53@gmail.com>
2022-11-18 19:33:58 -0300

    feat: 3 - Palíndromos (Recursividade)

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,21 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    """
+    # test manual
+    >>> is_palindrome_recursive('ANA', 0, 2)
+    True
+    >>> is_palindrome_recursive('AGUA', 0, 3)
+    False
+
+    :param word:
+    :param low_index:
+    :param high_index:
+    :return:
+    """
+    if not word:
+        return False
+    elif word[low_index] != word[high_index]:
+        return False
+    elif low_index >= high_index:
+        return True
+
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
