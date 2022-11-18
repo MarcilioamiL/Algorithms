c61bb6c27bc06927e61c07b413da3562847e3ceb
MarcilioamiL <marcilioaugusto53@gmail.com>
2022-11-18 18:56:06 -0300

    feat: 1 - Número de estudantes estudando no mesmo horário (Algoritmo de busca)

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,18 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    """
+    # test manual schedule
+    >>> study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 1)
+    2
+
+    :param permanence_period:
+    :param target_time:
+    :return:
+    """
+    users = 0
+    try:
+        for time_start, time_end in permanence_period:
+            if time_start <= target_time <= time_end:
+                users += 1
+        return users
+    except TypeError:
+        return None
