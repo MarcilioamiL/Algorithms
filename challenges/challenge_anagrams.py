23251f69c56785a03468aec423cb35375cf635fa
MarcilioamiL <marcilioaugusto53@gmail.com>
2022-11-20 19:54:49 -0300

    feat: 4 - Anagramas (Algoritmo de ordenação)

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,14 @@
-def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def anagrama(string):
+    word = ''
+    while len(string) > 0:
+        word += min(string)
+        string = string.replace(min(string), '', 1)
+
+    return word
+
+
+def is_anagram(string1, string2):
+    string1 = anagrama(string1).lower()
+    string2 = anagrama(string2).lower()
+
+    return (string1, string2, string1 == string2)
