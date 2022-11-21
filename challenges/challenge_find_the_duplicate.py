7144eaca55339ad54884ebb8c0c9954e2080a04b
MarcilioamiL <marcilioaugusto53@gmail.com>
2022-11-20 23:21:15 -0300

    feat: 5 - Encontrando números repetidos (Algoritmo de busca)

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,16 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    """
+    >>> find_duplicate([1, 3, 4, 2, 2])
+    2
+    >>> find_duplicate([3, 1, 2, 4, 6, 5, 7, 7, 7, 8])
+    7
+
+    :param num:
+    :return:
+    """
+    for num in nums:
+        if type(num) != int or num < 0:
+            return False
+        if nums.count(num) > 1:
+            return num
+    return False
